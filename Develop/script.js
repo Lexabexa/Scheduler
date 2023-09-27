76d4829d814a1525aade7b55a755ff53bc9beca0
lexabexa <lexabexa3@gmail.com>
2023-09-27 04:19:00 -0700

    clean p js for query

--- Develop/script.js
+++ Develop/script.js
@@ -2,50 +2,43 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function() {
-    //listener for click events on the save button. 
-    const saveButtons = document.querySelectorAll('.saveBtn');
-    saveButtons.forEach(saveButton => {
-    saveButton.addEventListener('click', function() {
-    const description = this.parentNode.querySelector('.description').value;
-    const timeBlockId = this.parentNode.id;
+  //current date in the header of the page
+  const currentDate = new Date();
+  const header = document.querySelector('header');
+  const dateDisplay = document.createElement('p');
+  dateDisplay.textContent = $('<p>').text(`Today is ${currentDate.toLocaleDateString()} and the time is ${currentDate.toLocaleTimeString()}.`);
+  header.append(dateDisplay);
+   //save button function to save to local storage 
+   $(".saveBtn").click(function() {
+    const description = $(this).siblings('.description').val();
+    const timeBlockId = $(this).parent().attr("id");
     localStorage.setItem(timeBlockId, description);
+   });
+    //listener for click events on the save button. 
+   $('.description').each(function())
+    const timeBlockId = $(this).parent().attr('id');
+    const savedDescription = localStorage.getItem(timeBlockId);
+    if (savedDescription) {
+      $(this).val(savedDescription);
+     }
     });
     
     
     // the past, present, or future class to each time
     // block by comparing the id to the current hour.
     const currentHour = dayjs().hour();
-
-   const timeBlocks = document.querySelectorAll('.time-block');
-   timeBlocks.forEach(timeBlock => {
-    const timeBlockHour = parseInt(timeBlock.id);
-    if (timeBlockHour < currentHour) {
-    timeBlock.classList.add('past');
-    } else if (timeBlockHour === currentHour) {
-    timeBlock.classList.add('present');
-    } else {
-    timeBlock.classList.add('future');
-    }
-   });
-  
-    //current date in the header of the page
-    const currentDate = new Date();
-    const header = document.querySelector('header');
-    const dateDisplay = document.createElement('p');
-    dateDisplay.textContent = `Today is ${currentDate.toLocaleDateString()} and the time is ${currentDate.toLocaleTimeString()}.`;
-    header.appendChild(dateDisplay);
-
-
-  //save button function to save to local storage 
-  $(".saveBtn").on("click", function() {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, text);
-  });
-
+    $(".time-block").each(function() {
+      const timeBlockHour = parseInt($(this).attr("id"));
+      if (timeBlockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+    
   //refresh storage
-  $("#Refresh").click(function(event) {
-    event.preventDefault();
-    $("textarea").val("");
-    localStorage.clear();
+  $("#Refresh").click(function() {
+   location.reload();
   });
