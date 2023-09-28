ffd6289c2597a49e813c2977e85ae106ee2448a3
lexabexa <lexabexa3@gmail.com>
2023-09-27 21:35:38 -0700

    editingdate/time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,19 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(document).ready(function() {
-  //current date in the header of the page
-  const currentDate = new Date();
-  const header = document.querySelector('header');
-  const dateDisplay = document.createElement('p');
-  dateDisplay.textContent = $('<p>').text(`Today is ${currentDate.toLocaleDateString()} and the time is ${currentDate.toLocaleTimeString()}.`);
-  header.append(dateDisplay);
+  //display current day and time
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+  $("#currentTime").text(dayjs().format("hh:mm:ss a"));
+});
   
   //save button function to save to local storage 
   $(".saveBtn").click(function() {
-    const description = $(this).siblings('.description').val();
+    const description = $(this).siblings(".description").val();
     const timeBlockId = $(this).parent().attr("id");
     localStorage.setItem(timeBlockId, description);
   });
   
   //listener for click events on the save button. 
-  $('.description').each(function(){
+  $(".description").each(function(){
     const timeBlockId = $(this).parent().attr('id');
     const savedDescription = localStorage.getItem(timeBlockId);
     if (savedDescription) {
@@ -47,4 +43,3 @@ $(document).ready(function() {
       $(".description").val("");
     });
   });
-});
